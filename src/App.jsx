613771f09b2fd39aed32c5056bb78758c15f6bf2
dasom222g <dasom228@gmail.com>
2025-07-16 16:04:25 +0900

    feat: Set firebase initial Add google login

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,30 @@ import Post from "./pages/Post";
 import Profile from "./pages/Profile";
 import Edit from "./pages/Edit";
 import Sample from "./pages/Sample";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 function App() {
   // logic
+  // 진입시 무조건 로딩. 인증서비스 준비 완료후에 로딩 false
+  const [isLoading, setIsLoading] = useState(true)
+
+  const init = async () => {
+    // firebase에서 로그인 데이터를 가져옴
+    await auth.authStateReady() // 로그인 상태 변화 감지
+    console.log("인증 완료", auth)
+    setIsLoading(false)
+  }
+
+  // 페이지 진입후 딱 한번 실행함
+  useEffect(() => {
+    init()
+  }, [])
 
   // view
   return (
     <div className="bg-churead-black h-full text-white overflow-auto">
-      <div className="max-w-[572px] mx-auto h-full">
+      {isLoading ? <p className="text-2xl">Loading..</p> : <div className="max-w-[572px] mx-auto h-full">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -25,7 +41,9 @@ function App() {
             <Route path="/sample" element={<Sample />} />
           </Routes>
         </BrowserRouter>
-      </div>
+      </div>}
+
+
     </div>
   );
 }
